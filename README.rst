b1b2e4459cf83dd2105cc4eaefe54e8ee3f8214b
m <mmailm@mail.ru>
2018-03-11 17:19:28 +0800

    add setup.py

---
+++ README.rst
@@ -0,0 +1,17 @@
+Django-Peewee
+=============
+
+About
+-----
+
+Django application which adds support for [Peewee ORM](http://docs.peewee-orm.com/en/latest/).
+
+
+Install
+-------
+
+
+Usage
+-----
+
+Add application to
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os.path
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setupconf = dict(
+    name='django-peewee',
+    version="0.0.1",
+    license='BSD',
+    url='https://github.com/SevenLines/django-peewee/',
+    author='Mikhail Katashevtsev',
+    author_email='fables@yandex.ru',
+    description=('Peewee to Django integration library'),
+    long_description=read('README.rst'),
+    packages=find_packages(),
+
+    install_requires=['peewee>=3.0.1'],
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+
+    # package_dir={'django_peewee': 'django_peewee'},
+)
+
+if __name__ == '__main__':
+    setup(**setupconf)
